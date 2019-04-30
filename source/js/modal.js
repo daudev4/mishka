296348b47b94f205232229364a33d3b93839f8a3
Daud <daud_08@mail.ru>
2019-04-30 23:49:21 +0300

    Оптимизировал проект. Оживил меню и модалку, добавил карту, исправил недочеты

--- source/js/modal.js
+++ source/js/modal.js
@@ -1,2 +1,22 @@
 var modal = document.querySelector(".modal");
-    modalOpen = document.querySelectorAll(".modal-open");
+    modalButtons = document.querySelectorAll(".modal-open");
+
+var revealModal = function (button) {
+  button.addEventListener("click", function(evt) {
+    evt.preventDefault();
+    modal.classList.add("modal_show");
+  })
+}
+
+for(var i = 0; i < modalButtons.length; i++) {
+  revealModal(modalButtons[i]);
+}
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (modal.classList.contains("modal_show")) {
+      modal.classList.remove("modal_show");
+    }
+  }
+});
