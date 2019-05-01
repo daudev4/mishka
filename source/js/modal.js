1c923adce7d6ff4cd13bec66c81996e93f4ddc0b
Daud <daud_08@mail.ru>
2019-05-01 13:46:11 +0300

    Переделал модальное окно

--- source/js/modal.js
+++ source/js/modal.js
@@ -1,22 +1,49 @@
-var modal = document.querySelector(".modal");
-    modalButtons = document.querySelectorAll(".modal-open");
+var isModalWindowOpen = false;
 
-var revealModal = function (button) {
-  button.addEventListener("click", function(evt) {
-    evt.preventDefault();
-    modal.classList.add("modal_show");
-  })
+var modalWrapper = document.querySelector('.js-modal__wrapper');
+var modalWindow = modalWrapper.querySelector('.js-modal__window');
+var modalOpenButtons = document.querySelectorAll('.js-modal__open'); // array
+var modalCloseButtons = modalWrapper.querySelectorAll('.js-modal__close'); // array
+
+function toggleModalWindow(value) { // boolean
+    isModalWindowOpen = value;
+
+    if (isModalWindowOpen) {
+        modalWrapper.classList.remove('modal__wrapper_hidden');
+    } else {
+        modalWrapper.classList.add('modal__wrapper_hidden');
+    }
 }
 
-for(var i = 0; i < modalButtons.length; i++) {
-  revealModal(modalButtons[i]);
-}
+modalOpenButtons.forEach(function(arrayItem){
+    arrayItem.addEventListener('click', function(evt) {
+        evt.preventDefault();
+        toggleModalWindow(true);
+    })
+});
+
+modalCloseButtons.forEach(function(arrayItem){
+    arrayItem.addEventListener('click', function(evt) {
+        evt.preventDefault();
+        toggleModalWindow(false);
+    })
+});
+
+document.addEventListener('click', function(evt) {
+    if (
+        isModalWindowOpen &&
+        evt.target.contains(modalWindow) &&
+        evt.target !== modalWindow
+    ) {
+        toggleModalWindow(false);
+    }
+})
 
 window.addEventListener("keydown", function (evt) {
   if (evt.keyCode === 27) {
     evt.preventDefault();
-    if (modal.classList.contains("modal_show")) {
-      modal.classList.remove("modal_show");
+    if (isModalWindowOpen) {
+      modalWrapper.classList.add("modal__wrapper_hidden");
     }
   }
 });
